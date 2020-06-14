c37c5adfdec3499ac6b14bc33b507d8f33d7dd68
Toreole <toreole.official@gmail.com>
2020-06-14 18:09:36 +0200

    WIP Editor for LaunchPad

--- Assets/Scripts/LaunchPad/Editor/LaunchPadEditor.cs
+++ Assets/Scripts/LaunchPad/Editor/LaunchPadEditor.cs
@@ -7,9 +7,43 @@ namespace UEGP3CA.Edit
     [CustomEditor(typeof(LaunchPad))]
     public class LaunchPadEditor : Editor 
     {
+        SerializedProperty vectorProperty;
+        Transform transform;
+        private void OnEnable() 
+        {
+            vectorProperty = serializedObject.FindProperty("launchVelocity");
+            transform = (target as MonoBehaviour).transform;
+        }
+
         private void OnSceneGUI() 
         {
-            var pad = target as LaunchPad;    
+            //change the launch setting.
+            Vector3 launchSpeed = Handles.DoPositionHandle(transform.position + vectorProperty.vector3Value, transform.rotation) - transform.position;
+            
+            //Handles.DrawLines(Vector3[])
+            vectorProperty.vector3Value = launchSpeed;
+            serializedObject.ApplyModifiedProperties();
+            //helpers.
+            float g = Mathf.Abs(Physics.gravity.y);
+            Vector3 xzSpeed = new Vector3(launchSpeed.x, 0, launchSpeed.z);
+            //1. calculate flight time.
+            float halfT = launchSpeed.y / g;
+            //2. figure out highest point. 
+            Vector3 highPoint = transform.position;
+            highPoint.y += Mathf.Pow(launchSpeed.y, 2) / (2*g);
+            highPoint += xzSpeed * halfT;
+            //3. get the endpoint.
+            Vector3 endPoint = transform.position + xzSpeed * (2*halfT);
+            //4. tangents.
+            var tanL = launchSpeed.magnitude / Mathf.PI;
+            var startTan = launchSpeed.normalized * tanL;
+            var midTan = xzSpeed.normalized * tanL;
+            var endTan = startTan;
+                endTan.y *= -1f;
+            //draw a preview of how it flies.
+            Handles.DrawBezier(transform.position, highPoint, transform.position + startTan, highPoint - midTan, Color.cyan, Texture2D.whiteTexture, 1);
+            Handles.DrawBezier(highPoint, endPoint, highPoint+midTan, endPoint-endTan, Color.cyan, Texture2D.whiteTexture, 1);
+
         }
     }
 }
